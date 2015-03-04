dda2ca063355cec132886e44308dbd27edee448f
Iain Reid <iainreid820@users.noreply.github.com>
2015-03-04 16:15:09 +0000

    Read function updated
    Updated and modified the read function. More code clean up has also
    taken place.

--- index.js
+++ index.js
@@ -1,37 +1,45 @@
-/*  TTL cache:
+/** TTL cache:
  *
  */
 var cache = {
   memory: {}
 };
 
-/*  Save to cache:
+/** Save to cache:
  *
  *  @param  identifier  {string}
  *  @param  data        {string}
+ *  @param  ttl         {int}       optional
  *  @param  callback    {function}
  */
 cache.write = function (identifier, data, ttl, callback) {
-  this.memory[identifier] = data;
+  this.memory[identifier] = {
+    identifier: identifier,
+    data: data,
+    ttl: ttl ? ttl : null,
+    ttlStart: ttl ? new Date().getTime() : null
+  };
   
-  if (ttl && typeof ttl === 'int') setTimeout(function () {
+  if (ttl && typeof ttl === 'number') setTimeout(function () {
     delete this.memory[identifier]
   }.bind(this), ttl);
   
   if (callback) callback()
 };
 
-/*  Read from cache:
+/** Read from cache:
  *
  *  @param  identifier  {string}
- *  @param  callback    {function}
+ *  @param  callback    {function}  optional - upon reading the cache, two arguements are returned, an error and a data object
  */
 cache.read = function (identifier, callback) {
-  if (callback) callback(this.memory[identifier]);
-  else return this.memory[identifier]
+  if (callback) callback(this.memory[identifier] ? null : {
+    message: 'Data at the specified identifier code not be found'
+  }, this.memory[identifier] ? this.memory[identifier].data : null);
+  else if (this.memory[identifier]) return this.memory[identifier].data;
 };
 
-/*  Delete from cache:
+/** Delete from cache:
  *
  *  @param  identifier  {string}
  *  @param  callback    {function}
@@ -42,7 +50,7 @@ cache.delete = function (identifier, callback) {
   if (callback) callback()
 };
 
-/*  Destroy cache:
+/** Destroy cache:
  *
  *  @param  callback  {function}
  */
